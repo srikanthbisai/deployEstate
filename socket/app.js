9d8ee0357067710ade0011081761dab7bb920062
srikanthbisai <srikanthbisai2110@gmail.com>
2024-07-28 18:55:06 +0530

    env update

--- socket/app.js
+++ socket/app.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 const io = new Server({
   cors: {
-    origin: process.env.CLIENT_URL // Adjust this if necessary
+    origin: process.env.CLIENT_URL 
   },
 });
 
