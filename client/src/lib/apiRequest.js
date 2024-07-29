663f9288cbf569568016e9c8fba7a566f22245d5
Srikanth Bisai <94598179+srikanthbisai@users.noreply.github.com>
2024-07-29 16:03:59 +0530

    Update apiRequest.js

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -7,4 +7,17 @@ const apiRequest = axios.create({
     withCredentials: true,
 });
 
+apiRequest.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 export default apiRequest;
